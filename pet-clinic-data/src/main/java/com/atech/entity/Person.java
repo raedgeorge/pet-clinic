d49cb51e92e8d5c4702f3dd7c1b2e3d3358e70d7
raedgeorge <raedgeorge2014@gmail.com>
2022-01-26 21:22:37 +0200

    added id property. closes #10.

--- pet-clinic-data/src/main/java/com/atech/entity/Person.java
+++ pet-clinic-data/src/main/java/com/atech/entity/Person.java
@@ -1,6 +1,6 @@
 package com.atech.entity;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/atech/entity/PetType.java
+++ pet-clinic-data/src/main/java/com/atech/entity/PetType.java
@@ -1,6 +1,6 @@
 package com.atech.entity;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
