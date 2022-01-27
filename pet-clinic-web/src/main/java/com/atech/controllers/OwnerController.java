ff0158ef38a947ef89a48ca89e8ef18fd19bb4c9
raedgeorge <raedgeorge2014@gmail.com>
2022-01-27 14:34:38 +0200

    created Vet index and controller. created Owner index and controller. closes #7. closes #13.

---
+++ pet-clinic-web/src/main/java/com/atech/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.atech.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
+    public String getOwnerList(){
+
+        return "owner/index";
+    }
+}
