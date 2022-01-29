e595f9dd4bd405adb433c74636756853fe8b814e
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-01-29 19:29:41 +0200

    List all vets. Closes #15

--- pet-clinic-web/src/main/java/com/atech/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/atech/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.atech.controllers;
 
+import com.atech.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/",  "/index", "/index.html"})
-    public String getVetsList(){
+    public String getVetsList(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
