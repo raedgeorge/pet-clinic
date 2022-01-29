ebbcab9e9ae80ab8fd3c87b172ed0e7fc31ab276
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-01-29 18:45:22 +0200

    List all owners on page. Close #16

--- pet-clinic-web/src/main/java/com/atech/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/atech/controllers/OwnerController.java
@@ -1,14 +1,31 @@
 package com.atech.controllers;
 
+import com.atech.entity.Owner;
+import com.atech.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String getOwnerList(){
+    public String getOwnerList(Model model){
+
+        List<Owner> ownerList = ownerService.findAll();
+
+        model.addAttribute("owners", ownerList);
 
         return "owner/index";
     }
