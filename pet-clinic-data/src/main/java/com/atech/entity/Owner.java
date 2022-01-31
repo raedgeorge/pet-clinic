34dace3d6a9b2f32b820dfd4b2cc78cae333d5be
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-01-31 10:52:52 +0200

    closes issue #26. adding Visit Entity, updating Pet and Pet Type

--- pet-clinic-data/src/main/java/com/atech/entity/Owner.java
+++ pet-clinic-data/src/main/java/com/atech/entity/Owner.java
@@ -1,4 +1,16 @@
 package com.atech.entity;
 
+import java.util.List;
+
 public class Owner extends Person{
+
+    private List<Pet> petList;
+
+    public List<Pet> getPetList() {
+        return petList;
+    }
+
+    public void setPetList(List<Pet> petList) {
+        this.petList = petList;
+    }
 }
