b18cc4a3c7580548207fd295cf009b1ce73dcb65
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-01-31 14:31:03 +0200

    closes #31. implement SpecialityMapService

---
+++ pet-clinic-data/src/main/java/com/atech/service/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.atech.service;
+
+import com.atech.entity.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/atech/service/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.atech.service.map;
+
+import com.atech.entity.Speciality;
+import com.atech.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public List<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
