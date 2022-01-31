8b9cb72acc63c063ba81f513e285daf93e133c8e
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-01-31 11:07:19 +0200

    closes issue #27. added Speciality entity, associated to Vet

--- pet-clinic-data/src/main/java/com/atech/entity/Vet.java
+++ pet-clinic-data/src/main/java/com/atech/entity/Vet.java
@@ -1,5 +1,16 @@
 package com.atech.entity;
 
+import java.util.List;
+
 public class Vet extends Person{
 
+    private List<Speciality> specialities;
+
+    public List<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(List<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
