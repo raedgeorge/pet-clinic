5feeb8662ce0824da952fc719f4a309a6890d143
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-02-03 20:51:48 +0200

    closes #36. updated Owner, Pet, PetTyp with JPA Entity Manager

--- pet-clinic-data/src/main/java/com/atech/entity/Owner.java
+++ pet-clinic-data/src/main/java/com/atech/entity/Owner.java
@@ -1,15 +1,26 @@
 package com.atech.entity;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
     private List<Pet> petList = new ArrayList<>();
 
+    // Getters and Setters
     public String getAddress() {
         return address;
     }
--- pet-clinic-data/src/main/java/com/atech/entity/Pet.java
+++ pet-clinic-data/src/main/java/com/atech/entity/Pet.java
@@ -1,14 +1,30 @@
 package com.atech.entity;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,
+                          CascadeType.REFRESH,
+                          CascadeType.PERSIST,
+                          CascadeType.MERGE})
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    // Getters and Setters
     public PetType getPetType() {
         return petType;
     }
