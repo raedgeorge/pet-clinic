ab7e319a488cbd4b9d209bd9704156004593ccf0
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-02-16 09:30:15 +0200

    implemented ownerDetails.html closes #53

--- pet-clinic-web/src/main/java/com/atech/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/atech/controllers/OwnerController.java
@@ -2,10 +2,13 @@ package com.atech.controllers;
 
 import com.atech.entity.Owner;
 import com.atech.service.OwnerService;
+import com.atech.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -14,27 +17,47 @@ import java.util.List;
 public class OwnerController {
 
     private final OwnerService ownerService;
+    private final PetService petService;
 
     @Autowired
-    public OwnerController(OwnerService ownerService) {
+    public OwnerController(OwnerService ownerService,
+                           PetService petService) {
+
         this.ownerService = ownerService;
+        this.petService = petService;
     }
 
-    @RequestMapping({"","/", "/index", "/index.html"})
+    @RequestMapping({"","/", "/find", "/ownersList.html"})
     public String getOwnerList(Model model){
 
         List<Owner> ownerList = ownerService.findAll();
 
         model.addAttribute("owners", ownerList);
 
-        return "owners/index";
+        return "owners/ownersList";
     }
 
-    @RequestMapping("/find")
-    public String findOwners(Model theModel){
+    // owner detail. implementation 1
+//    @RequestMapping("/{ownerId}/detail")
+//    public String ownerDetail(
+//            Model theModel,
+//            @PathVariable("ownerId") long ownerId){
+//
+//        Owner owner = ownerService.findById(ownerId);
+//
+//        theModel.addAttribute("owner", owner);
+//
+//        return "owners/ownerDetails";
+//    }
 
-        theModel.addAttribute("owners", ownerService.findAll());
+    // owner detail implementation 2
+    @RequestMapping("/{ownerId}/detail")
+    public ModelAndView ownerDetail2(@PathVariable("ownerId") long ownerId){
 
-        return "notimplemented";
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+
+        modelAndView.addObject(ownerService.findById(ownerId));
+
+        return modelAndView;
     }
 }
