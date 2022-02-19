af9fd4ab723882d1c8650c5535a7eb946a756b2d
raedgeorge2014@gmail.com <raedgeorge2014@gmail.com>
2022-02-19 13:20:13 +0200

    added vet list endpoint for JSON closed #63

--- pet-clinic-web/src/main/java/com/atech/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/atech/controllers/VetController.java
@@ -1,13 +1,17 @@
 package com.atech.controllers;
 
+import com.atech.entity.Vet;
 import com.atech.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class VetController {
 
     private final VetService vetService;
@@ -24,5 +28,10 @@ public class VetController {
         vetService.findAll().forEach(vet -> System.out.println(vet));
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody List<Vet> getVetJson(){
+        return vetService.findAll();
     }
 }
